5f0028137573f1f611cf3abc3f6a43819154b60c
emmytheo <chidi.mgbara@gmail.com>
2021-08-02 09:47:31 +0100

    first Upload

--- public/themesbrand.com/minible/layouts/controller.js
+++ public/themesbrand.com/minible/layouts/controller.js
@@ -1,5 +1,8 @@
 // Establish a Socket.io connection
-const socket = io('http://127.0.0.1');
+
+
+
+const socket = io('http://' + window.location.host + ':80');
 // Initialize our Feathers client application through Socket.io
 // with hooks and authentication.
 const client = feathers();
